d7dca706d738c39570869a6f9d6a31558e0eabb0
Samuel Santos <mucasantos@icloud.com>
2021-10-21 20:21:55 -0300

    Primeiro programa
    First Swift 5 app

--- amrich/ViewController.swift
+++ amrich/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+           }
+
+    @IBAction func changeAlpha(_ sender: Any) {
+        
+        let images =   [ #imageLiteral(resourceName: "fruit3"),#imageLiteral(resourceName: "fruit6"),#imageLiteral(resourceName: "fruit2"),#imageLiteral(resourceName: "fruit1"),#imageLiteral(resourceName: "fruit9"),#imageLiteral(resourceName: "fruit5"),#imageLiteral(resourceName: "fruit7"),#imageLiteral(resourceName: "fruit8"),#imageLiteral(resourceName: "fruit4") ]
+        myImage.image = images[Int.random(in: 0...images.count)]
     }
-
-
+    
 }
 
